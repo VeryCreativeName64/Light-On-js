d0950bbb7b7a696928326e9cf85050b38d3fdd0a
VeryCreativeName64 <marekbendeguz@gmail.com>
2025-03-27 10:55:38 +0100

    Lampa importalas

---
+++ JatekTer.js
@@ -0,0 +1,19 @@
+import Lampa
+
+export default class JatekTer{
+    #lista=[0,0,0,0,0,0,0,0,0];
+    #lekapcsolt;
+    constructor(SzElem) {
+        this.#lista = lista;
+        this.#lekapcsolt = lekapcsolt;
+        this.SzElem=SzElem;
+        this.megjelenit();
+    }
+
+    megjelenit(){
+        for (let index = 0; index < this.#lista.length; index++) {
+            new 
+            
+        }
+    }
+}
